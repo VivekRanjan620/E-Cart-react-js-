89c28e7a691967a4be462a16011b52407d423150
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-04-01 19:24:53 +0530

    Shopping-Cart

---
+++ src/Components/Product.jsx
@@ -0,0 +1,119 @@
+// import React from 'react'
+// import { items } from './Data'
+
+// const Product = () => {
+//   return (
+//     <>
+//     <div classNameName="container my-5">
+//         <div classNameName="row">
+//         {
+//             items.map((product)=>{
+//                return(
+//                 <>
+//                 <div className="col-lg-4 my-3 text-center">
+//                 <div className="card" style={{width:'18rem'}}>
+//   <img src={product.imgSrc} className="card-img-top" alt="..."/>
+//   <div className="card-body">
+//     <h5 className="card-title">Card title</h5>
+//     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     <a href="#" className="btn btn-primary">Go somewhere</a>
+//   </div>
+// </div>
+// </div>
+//                 </>
+//                )
+//             })
+//         }
+//     </div>
+//     </div>
+    
+//     </>
+//   )
+// }
+
+// export default Product
+
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Product = ({items, cart , setCart}) => {
+   
+  const addToCart = (id,price,title,description,imgSrc) =>{
+    const obj = {
+      id,price,title,description,imgSrc
+    }
+    setCart([...cart, obj]);
+    console.log("Cart element = ",cart)
+    toast.success('Item added on cart', {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
+  }
+
+
+  return (
+    <>
+    <ToastContainer
+position="top-right"
+autoClose={1500}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="dark"
+/>
+      <div className="container my-5">
+        <div className="row">
+          {items.map((product) => {
+            return (
+              <>
+                <div key={product.id} className="col-lg-4 col-md-6 my-3 text-center">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <Link to={`/product/${product.id}`}
+                     style={{
+                        display:'flex',
+                        justifyContent:'center',
+                        alignItems:'center'
+                    }}>
+
+                    <img
+                      src={product.imgSrc}
+                      className="card-img-top"
+                      alt="..."
+                      />
+                      </Link>
+                    <div className="card-body">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">{product.description}</p>
+                      <button className="btn btn-primary mx-3">
+                        {product.price} ₹
+                      </button>
+                      <button
+                      onClick={()=>addToCart(product.id,product.price,product.title,product.description,product.imgSrc)}
+                       className="btn btn-warning"
+                       >Add To Cart</button>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
